bf3ad5280df7f2a3c53bd7451368a58a6bc18c6d
Boom Flex <jake182@gmail.com>
2025-06-06 12:29:25 +0100

    update script

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ async function analyze() {
   const userA = document.getElementById('userA').value;
   const userB = document.getElementById('userB').value;
 
-  const response = await fetch("https://ba31-2a02-c7c-584a-a900-d566-912a-fc31-585b.ngrok-free.app/analyze", {
+fetch("https://ba31-2a02-c7c-584a-a900-d566-912a-fc31-585b.ngrok-free.app/analyze", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+        "Content-Type": "application/json"
     },
-    body: JSON.stringify({ user_a: userA, user_b: userB })
-  });
+    body: JSON.stringify({
+        user_a: userALogs,
+        user_b: userBLogs
+    })
+})
 
   const data = await response.json();
 
